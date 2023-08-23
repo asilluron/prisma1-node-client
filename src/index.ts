36c8567529a31baa49df554431bf5ea7e2f3c14d
Andrew Silluron <asilluron@users.noreply.github.com>
2023-08-22 23:13:29 -0600

    remove TS definitions BS

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,4 @@ export { ClientOptions, BaseClientOptions, Model } from './types';
 export { Generator } from './codegen/Generator';
 export { JavascriptGenerator } from './codegen/generators/javascript-client';
 export { TypescriptGenerator } from './codegen/generators/typescript-client';
-export {
-  TypescriptDefinitionsGenerator,
-} from './codegen/generators/typescript-definitions';
 export { makePrismaClientClass } from './makePrismaClientClass';
